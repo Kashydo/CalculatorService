659ba005928531ccb9ea76db2f1c7174c504b5c5
Kashydo <kaszydo@gmail.com>
2024-01-27 10:43:47 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CalculatorService
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
