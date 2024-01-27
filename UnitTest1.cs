0696813ac6436fa6aae63cf3e7fd6067043bb1a1
Kashydo <kaszydo@gmail.com>
2024-01-27 11:03:23 +0100

    feat: added classes for CalculatorService and DivideResylt. Test don't pass

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,4 @@
+
 namespace Calculator
 {
     public class Tests
@@ -8,10 +9,25 @@ namespace Calculator
         [Test]
         public void WhenCalculatorDividesReturnDivideResult()
         {
-            DivideResult result = _calcularoService.Divide();
+            int x =5; int y =2;
+            DivideResult result = _calcularoService.Divide(x,y);
             Assert.NotNull(result);
-            Assert.That(result.Divide, Is.Not.Null);
-            Assert.That(result.Reminder.Is.Not.Null);
+            Assert.That(result.Divide, Is.EqualTo(2));
+            Assert.That(result.Reminder, Is.EqualTo(5));
         }   
+    }
+
+    internal class DivideResult
+    {
+        public int Divide { get; set; }
+        public int Reminder { get; set; }
+    }
+
+    internal class CalcularoService
+    {
+        internal DivideResult Divide(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
