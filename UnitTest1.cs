915201871aa8c467445786458e76bc42883f7966
Kashydo <kaszydo@gmail.com>
2024-01-27 10:58:25 +0100

    feat: Test for divide result

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,16 +1,17 @@
-namespace CalculatorService
+namespace Calculator
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        private readonly CalcularoService _calcularoService;
+        public Tests() { }
 
         [Test]
-        public void Test1()
+        public void WhenCalculatorDividesReturnDivideResult()
         {
-            Assert.Pass();
-        }
+            DivideResult result = _calcularoService.Divide();
+            Assert.NotNull(result);
+            Assert.That(result.Divide, Is.Not.Null);
+            Assert.That(result.Reminder.Is.Not.Null);
+        }   
     }
 }
